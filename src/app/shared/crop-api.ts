218d32f64904357947bb7cc8f2efc189a9f2c6f2
Sallai András <andrewteki@gmail.com>
2025-11-27 11:27:57 +0100

    Szolgáltatások kész; crop.component folytatva

--- src/app/shared/crop-api.ts
+++ src/app/shared/crop-api.ts
@@ -7,16 +7,6 @@ import { environment } from '../../environments/environment.development';
   providedIn: 'root',
 })
 export class CropApi {
-  crops = [
-    { id: 1, name: 'Búza', type: 'sárga', proteinContent: 20.2, producerId: 1 },
-    { id: 2, name: 'Köles', type: 'sárga', proteinContent: 20.2, producerId: 1 },
-    { id: 3, name: 'Zab', type: 'sárga', proteinContent: 20.2, producerId: 1 },
-    { id: 4, name: 'Árpa', type: 'sárga', proteinContent: 20.2, producerId: 1 },
-    { id: 5, name: 'Zab', type: 'sárga', proteinContent: 12.2, producerId: 2 },
-    { id: 6, name: 'Köles', type: 'sárga', proteinContent: 23.2, producerId: 2 },
-    { id: 7, name: 'Rizs', type: 'sárga', proteinContent: 15.2, producerId: 2 },
-    { id: 8, name: 'Búza', type: 'sárga', proteinContent: 20.2, producerId: 2 },
-  ]
 
   host = environment.apiHost
 
--- src/app/shared/producer-api.ts
+++ src/app/shared/producer-api.ts
@@ -1,7 +1,33 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProducerApi {
+
+  host = environment.apiHost
+
+  constructor(private http: HttpClient) {}  
+
+  getProducers() {
+    const url = this.host + 'producers'
+    return this.http.get(url)
+  }
+
+  createProducer(producer: any) {
+    const url = this.host + 'producers'
+    return this.http.post(url, producer)
+  }
+
+  updateProducer(producer: any) {
+    const url = this.host + 'producers/' + producer.id
+    return this.http.put(url, producer)
+  }
+
+  deleteProducer(id: number) {
+    const url = this.host + 'producers/' + id
+    return this.http.delete(url)
+  }
 }
