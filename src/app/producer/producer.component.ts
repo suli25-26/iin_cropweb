9bd24b4c1f78e5558166f747af047b5a74dcaba2
Erős István <eros@valahol>
2025-11-28 09:30:40 +0100

    Producer read művelet működik; create elkezdve

--- src/app/producer/producer.component.ts
+++ src/app/producer/producer.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { ProducerApi } from '../shared/producer-api';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-producer',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './producer.component.html',
   styleUrl: './producer.component.css',
 })
 export class ProducerComponent {
 
+  producers!: any;
+  producerForm!: any;
+  addMode = true;
+
+  constructor(
+    private api: ProducerApi,
+    private builder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.producerForm = this.builder.group({
+      id: '',
+      name: '',
+      country: '',
+      yearOfFoundation: '',
+      capacityHectare: ''
+    })
+    this.getProducers();
+  }
+
+  getProducers() {
+    this.api.getProducers().subscribe({
+      next: (res: any) => {
+        console.log(res)
+        this.producers = res.data
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+  }
+  save() {
+    if(this.addMode) {
+      this.addProducer()
+    }else {
+      this.updateProducer();
+    }
+  }
+  addProducer() {
+    console.log('Add ...')
+  }
+  updateProducer() {}
+  editProducer() {}
+  deleteProducer() {}
 }
